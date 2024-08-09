7fc1831548df3a36d9a76d77b2aeb58950b85432
jagadish7-25 <kadapajag7@gmail.com>
2024-08-09 11:13:15 +0200

    sg updated

--- server.tf
+++ server.tf
@@ -1,6 +1,7 @@
 resource "aws_instance" "jacks" {
   ami           = "ami-03972092c42e8c0ca"
   instance_type = "t2.micro"
+  vpc_security_group_ids = ["sg-09c956a01686bdc1d"] 
   count         = 1
 
   tags = {
