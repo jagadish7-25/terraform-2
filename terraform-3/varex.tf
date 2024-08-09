ffdb841c67cc5ceb46e90c7d401dc3b4e4285988
jagadish7-25 <kadapajag7@gmail.com>
2024-08-09 11:53:29 +0200

    folders added

---
+++ terraform-3/varex.tf
@@ -0,0 +1,12 @@
+resource "aws_instance" "jacks" {
+  ami           = "ami-03972092c42e8c0ca"
+  instance_type = var.instance_type
+  vpc_security_group_ids = ["sg-09c956a01686bdc1d"] 
+  count         = 1
+
+  tags = {
+    Name = var.Name
+  }
+}
+
+
